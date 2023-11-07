4eb38dbed4f644582027b03e166e593de1f2783a
toch <tocha.mateusz+main@gmail.com>
2023-11-07 19:38:38 +0100

    feat: implement basic controllers

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpRequest, HttpServer, Responder};
+
+#[get("/")]
+async fn get_users(_req: HttpRequest) -> impl Responder {
+    "Get Users"
+}
+
+#[get("/login")]
+async fn login(_req: HttpRequest) -> impl Responder {
+    "Login"
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| App::new().service(get_users).service(login))
+        .bind(("127.0.0.1", 8080))?
+        .run()
+        .await
 }
